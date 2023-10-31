365af991f12570228e74e49508992e933600c4f5
Jimson <97458912+Jims0n@users.noreply.github.com>
2023-10-31 15:49:42 +0100

    Update race name

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ import Head from "next/head";
 import Link from "next/link";
 
 
-export const grandPrix = "FORMULA 1 GRAN PREMIO DE LA CIUDAD DE MÉXICO 2023";
+export const grandPrix = "FORMULA 1 ROLEX GRANDE PRÊMIO DE SÃO PAULO 2023";
 
 type HomeContainerProps = {
   isLeaderboardOpen: boolean;
